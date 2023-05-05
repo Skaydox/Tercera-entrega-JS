2cbc3b4cc793d02cb3d73341546e693d56b01375
Skaydox <juliantw3012@gmail.com>
2023-05-05 16:51:10 -0300

    primeras versiones

---
+++ main.js
@@ -0,0 +1,6 @@
+let nombre = document.getElementById("nombre")
+let email = document.getElementById("email")
+let contra = document.getElementById("contra")
+
+let info = document.getElementById("info")
+let recordar = document.getElementById("recordar")
