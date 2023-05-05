75bd972d676a0de7184745854f18f408553791ba
Skaydox <juliantw3012@gmail.com>
2023-05-05 17:33:04 -0300

    finalizando el proyecto

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-let nombre = document.getElementById("nombre")
-let email = document.getElementById("email")
-let contra = document.getElementById("contra")
+let form = document.getElementById("formulario")
 
-let info = document.getElementById("info")
-let recordar = document.getElementById("recordar")
+let nombre = document.getElementById("nombre");
+let email = document.getElementById("email");
+let contra = document.getElementById("contra");
+
+let info = document.getElementById("info");
+let recordar = document.getElementById("recordar");
+
+
+nombre.value = localStorage.getItem("nombre")
+email.value = localStorage.getItem("email")
+contra.value = sessionStorage.getItem("contrasena")
+
+form.addEventListener("change",() => {
+    if(recordar.checked){
+        localStorage.setItem("nombre",nombre.value);
+        localStorage.setItem("email",email.value);
+        sessionStorage.setItem("contrasena",contra.value);
+    }
+})
+
+recordar.addEventListener("click",() => {
+    recordar.checked ? info.innerHTML = `¡Tus datos se guardaran automaticamente!<br>
+                                        <span class="warn">(La contraseña se eliminará al cerrar la página)</span>` : info.innerHTML = ""
+})
