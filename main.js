5e0b2a39a5bc509af19f9fb6bc69ca9cf82767dc
Skaydox <juliantw3012@gmail.com>
2023-05-05 18:26:06 -0300

    terminado

--- main.js
+++ main.js
@@ -6,12 +6,14 @@ let contra = document.getElementById("contra");
 
 let info = document.getElementById("info");
 let recordar = document.getElementById("recordar");
+let eliminar = document.getElementById("eliminar");
 
-
+//Carga todos los datos si existen
 nombre.value = localStorage.getItem("nombre")
 email.value = localStorage.getItem("email")
 contra.value = sessionStorage.getItem("contrasena")
 
+//Guarda la información a la hora de cambiar de input
 form.addEventListener("change",() => {
     if(recordar.checked){
         localStorage.setItem("nombre",nombre.value);
@@ -20,7 +22,17 @@ form.addEventListener("change",() => {
     }
 })
 
+eliminar.addEventListener("click",(event) => {
+    event.preventDefault();
+    localStorage.clear();
+    sessionStorage.clear();
+    nombre.value = "";
+    email.value = "";
+    contra.value = "";
+})
+
+//Modificación del DOM mediante el innterHTML para que te indique que es lo que va a suceder
 recordar.addEventListener("click",() => {
     recordar.checked ? info.innerHTML = `¡Tus datos se guardaran automaticamente!<br>
-                                        <span class="warn">(La contraseña se eliminará al cerrar la página)</span>` : info.innerHTML = ""
+                                        <span class="warn">(La contraseña se eliminará al cerrar la página)</span>` : info.innerHTML = "";
 })
